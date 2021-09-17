8f4891fb2b60022ec13cb89e5e30501c3c8eff75
Roman Glogowski <18666546+diablo39@users.noreply.github.com>
2021-09-17 22:41:03 +0200

    allow for any in typescript

--- src/ui/.eslintrc.js
+++ src/ui/.eslintrc.js
@@ -16,5 +16,6 @@ module.exports = {
     'no-debugger': process.env.NODE_ENV === 'production' ? 'warn' : 'off',
     'valid-v-slot': 'off',
     'vue/valid-v-slot': 'off',
+    "@typescript-eslint/no-explicit-any": "off",
   }
 }
