d8c5680aeb71afe8c18b9e9480da4733292d30d2
Roman Glogowski <18666546+diablo39@users.noreply.github.com>
2021-10-03 19:57:13 +0200

    datetime fixes

--- src/api/src/ApplicationRegistry.Infrastructure/UnitOfWork/ApplicationRegistryDatabaseContext.cs
+++ src/api/src/ApplicationRegistry.Infrastructure/UnitOfWork/ApplicationRegistryDatabaseContext.cs
@@ -6,6 +6,7 @@ using ApplicationRegistry.Domain.Entities.Network;
 using ApplicationRegistry.Domain.Entities.Redis;
 using ApplicationRegistry.Infrastructure.Properties;
 using Microsoft.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore.Storage.ValueConversion;
 using System;
 
 namespace ApplicationRegistry.Infrastructure.UnitOfWork
@@ -87,6 +88,34 @@ namespace ApplicationRegistry.Infrastructure.UnitOfWork
             base.OnModelCreating(modelBuilder);
 
             modelBuilder.ApplyConfigurationsFromAssembly(typeof(ApplicationRegistryDatabaseContext).Assembly);
+
+            var dateTimeConverter = new ValueConverter<DateTime, DateTime>(
+                v => v.ToUniversalTime(),
+                v => DateTime.SpecifyKind(v, DateTimeKind.Utc));
+
+            var nullableDateTimeConverter = new ValueConverter<DateTime?, DateTime?>(
+                v => v.HasValue ? v.Value.ToUniversalTime() : v,
+                v => v.HasValue ? DateTime.SpecifyKind(v.Value, DateTimeKind.Utc) : v);
+
+            foreach (var entityType in modelBuilder.Model.GetEntityTypes())
+            {
+                if (entityType.IsKeyless)
+                {
+                    continue;
+                }
+
+                foreach (var property in entityType.GetProperties())
+                {
+                    if (property.ClrType == typeof(DateTime))
+                    {
+                        property.SetValueConverter(dateTimeConverter);
+                    }
+                    else if (property.ClrType == typeof(DateTime?))
+                    {
+                        property.SetValueConverter(nullableDateTimeConverter);
+                    }
+                }
+            }
         }
     }
 }
