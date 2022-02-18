8b56170c311c8f69f4527ef444b55e27db2c6643
Roman Glogowski <18666546+diablo39@users.noreply.github.com>
2022-02-18 21:04:52 +0100

    migration

---
+++ src/api/src/ApplicationRegistry.Web/Migrations/20220218200439_RemoveAutorestDependency.cs
@@ -0,0 +1,25 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace ApplicationRegistry.Web.Migrations
+{
+    public partial class RemoveAutorestDependency : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DeleteData(
+                table: "DependencyType",
+                keyColumn: "Id",
+                keyValue: "AUTORESTCLIENT");
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.InsertData(
+                table: "DependencyType",
+                columns: new[] { "Id", "CanBeAddedManualy", "Name" },
+                values: new object[] { "AUTORESTCLIENT", false, "AutoRest Client" });
+        }
+    }
+}
